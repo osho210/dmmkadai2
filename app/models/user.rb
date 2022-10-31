d908f5a207717b5eb31118ae30999b208e356760
osho210 <anysense.yamatsu@gmail.com>
2022-10-31 06:33:43 +0000

    [Start] dmm2実践課題の開始

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  has_one_attached :profile_image
+  
+  # userが1になるので
+  has_many :book, dependent: :destroy
+  
+end
