4688fa2a610e2b3b6e98cc598de1bb5491a1afa0
osho210 <anysense.yamatsu@gmail.com>
2022-11-01 13:17:22 +0000

    [Add] 11/1日分

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,5 +6,11 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.new(book_params)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
